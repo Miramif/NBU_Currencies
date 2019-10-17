05327909c7f42ac6ebe477b7d0b19f14e9851020
Miramif <Miramif6801@gmail.com>
2019-10-17 18:38:37 +0300

    First source files. Included class for parsing xml files

---
+++ source/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net;
+using System.IO;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace CurrencyDisp
+{
+    public partial class Form1 : Form
+    {
+        bool XmlLoadedCorrectly = false;
+        XmlParser parser;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DateTime buf;
+                bool TryDate = DateTime.TryParse(textBox2.Text, out buf);
+            if (!TryDate) 
+            {
+                MessageBox.Show("Incorrect format of date. Should be dd.mm.yyyy. Date will be set to current.", "Date error", MessageBoxButtons.OK);
+                buf = DateTime.Now;
+                //label1.Text = buf.ToString("yyyyMMdd");
+            }
+
+            string path = "https://old.bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode="
+                + textBox1.Text + "&date=" + buf.ToString("yyyyMMdd");
+
+            byte[] data;
+            using (WebClient webClient = new WebClient())
+                data = webClient.DownloadData(path);
+
+            string str = Encoding.GetEncoding("Windows-1252").GetString(data);
+            XDocument XmlCur = XDocument.Parse(str);
+            foreach (XElement record in XmlCur.Element("exchange").Elements("currency"))
+            {
+
+                XElement name = record.Element("cc");
+                XElement ratio = record.Element("rate");
+                XElement date = record.Element("exchangedate");
+
+                if (name != null && ratio != null && date != null)
+                {
+                    parser = new XmlParser(name.Value, double.Parse(ratio.Value, CultureInfo.InvariantCulture),
+                         Convert.ToDateTime(date.Value));
+                    XmlLoadedCorrectly = true;
+                }
+                if (XmlLoadedCorrectly)
+                {
+                    label1.Text = parser.CurNameGS;
+                    label2.Text = parser.RatioGS.ToString();
+                    label3.Text = Convert.ToString(parser.DateGS.Date);
+                }
+            }
+
+        }
+    }
+}
---
+++ source/XmlParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Net;
+using System.IO;
+using System.Globalization;
+
+namespace CurrencyDisp
+{
+    class XmlParser
+    {
+        string CurName;
+        Double Ratio;
+        DateTime Date;
+        public XmlParser(string name, double ratio, DateTime date)
+        {
+                    CurName = name;
+                    Ratio = ratio;
+                    Date = date;
+        }
+        public string CurNameGS
+        {
+            get { return CurName; }
+            set { CurName = value; }
+        }
+        public double RatioGS
+        {
+            get { return Ratio; }
+            set { Ratio = value; }
+        }
+        public DateTime DateGS
+        {
+            get { return Date; }
+            set { Date = value; }
+        }
+    }
+}
